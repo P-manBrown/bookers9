311b671ce11f4923c5b12dd217ded9eb12452421
P-manBrown <tachikawa.yuya@outlook.jp>
2021-12-02 13:53:37 +0000

    [add]2-2-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,18 @@ class User < ApplicationRecord
   has_many :books
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
+
+	has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+	has_many :followings, through: :activ_relationships, source: :follower
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+	has_many :followers, through: :passive_relationships, source: :following
+	
   attachment :profile_image, destroy: false
+  
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
+  
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }

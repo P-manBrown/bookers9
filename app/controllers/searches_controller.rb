dbf6179c6f4d52fccc51181adcfa714dddd2414a
P-manBrown <tachikawa.yuya@outlook.jp>
2021-12-04 14:31:38 +0000

    [Finish]2-2-2

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,26 +1,16 @@
 class SearchesController < ApplicationController
   
+ before_action :authenticate_user!
+
   def search
-    @model = params["model"]
-    @method = params["method"]
-    @content = params["content"]
-    @records = search_for(@model, @content, @method)
-  end
-  
-  private
-  def search_for(model, content, method)
-    if model == 'user'
-      if method == 'perfect'
-        User.where(name: content)
-      else
-        User.where('name LIKE ?', '%' + content + '%')
-      end
-    elsif model == 'book'
-      if method == 'perfect'
-        Book.where(titile: content)
-      else
-        Book.where('title LIKE ?', '%' + content + '%')
-      end
+    @word = params[:word]
+    @range = params[:range]
+    if @range == "User"
+      @users = User.looks(params[:search], @word)
+    else
+      @books = Book.looks(params[:search], @word)
     end
   end
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,28 @@ class User < ApplicationRecord
 	has_many :followings, through: :active_relationships, source: :follower
 	has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
 	has_many :followers, through: :passive_relationships, source: :following
-	
+
   attachment :profile_image, destroy: false
-  
+
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
   end
-  
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
 end
